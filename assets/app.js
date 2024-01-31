31cd646d49c7ec974a903ba2ea581e505d2f5f58
kazuki2000 <pkazuki2000@icloud.com>
2024-01-31 22:33:46 +0900

    終了

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-import './bootstrap.js';
+import "./bootstrap.js";
 /*
  * Welcome to your app's main JavaScript file!
  *
@@ -7,7 +7,7 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import "./styles/app.css";
 
 // start the Stimulus application
-import './bootstrap';
+import "./bootstrap";
